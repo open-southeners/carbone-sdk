20b10c37127ab81e4f31ec830a9aa0a6dffb8943
Rubén Robles <me@d8vjork.com>
2023-12-26 23:55:05 +0100

    add phpstan type coverage extension

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,19 +2,16 @@
 
 namespace OpenSoutheners\CarboneSdk;
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->app->bind(Carbone::class, function () {
+        $this->app->bind(Carbone::class, function (): Carbone {
             /** @var string $key */
             $key = config('services.carbone.key', env('CARBONE_API_KEY', ''));
             /** @var string|null $version */
@@ -24,15 +21,5 @@ class ServiceProvider extends BaseServiceProvider
         });
 
         $this->app->alias(Carbone::class, 'carbone');
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -3,6 +3,8 @@
 namespace OpenSoutheners\CarboneSdk\Tests;
 
 use OpenSoutheners\CarboneSdk\Carbone;
+use OpenSoutheners\CarboneSdk\ServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 /**
  * @group integration
@@ -11,8 +13,23 @@ use OpenSoutheners\CarboneSdk\Carbone;
  */
 class ServiceProviderTest extends TestCase
 {
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string>
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
     public function testCarboneIsBoundWithCarboneInstance()
     {
+        $this->withoutExceptionHandling();
+
         $this->assertTrue($this->app->bound(Carbone::class));
         $this->assertTrue($this->app->bound('carbone'));
         $this->assertInstanceOf(Carbone::class, $this->app->get('carbone'));
